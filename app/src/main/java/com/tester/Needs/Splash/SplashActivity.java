e046008ca1f1eebad0bd10136bb588eb002dd1fd
Tae Sung Park <pts322@naver.com>
2020-02-27 12:43:38 +0900

    Add Google AdMob

--- app/src/main/java/com/tester/Needs/Splash/SplashActivity.java
+++ app/src/main/java/com/tester/Needs/Splash/SplashActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.android.gms.ads.MobileAds;
 import com.tester.Needs.Main.MainActivity;
 import com.tester.Needs.R;
 
@@ -15,6 +16,9 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        MobileAds.initialize(this, "ca-app-pub-1543630224731130~6261268123");
+
         startLoading();
     }
 
